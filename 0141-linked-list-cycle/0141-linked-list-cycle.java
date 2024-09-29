6419416010de03d816d6c1bb216fcf4f8affb93b
sravya97 <sravya.gopireddy09@gmail.com>
2024-09-29 16:22:46 -0500

    Time: 0 ms (100%), Space: 44 MB (88.78%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,8 +11,8 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
         ListNode fast = head;
+        ListNode slow = head;
         while(fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
@@ -21,6 +21,6 @@ public class Solution {
             }
         }
         return false;
-        
+
     }
 }
