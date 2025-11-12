49442384960e82dfeb1b090907d35d7e82bb7cea
sravya97 <sravya.gopireddy09@gmail.com>
2025-11-11 18:01:06 -0600

    Time: 2 ms (75.72%), Space: 55.6 MB (8.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //Moore Voting Algorithm
-        int candidate = 0;
-        int count = 0;
-        for(int i=0;i<nums.length;i++){
-            if(count <= 0){
-                candidate = nums[i];
-                count++;
+        //Moore voting algo
+        int candidate = nums[0];
+        int vote = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == candidate){
+                vote++;
             } else {
-                if(nums[i] != candidate){
-                    count--;
-                } else {
-                    count++;
-                }
+                vote --;
+            }
+            if(vote == 0){
+                candidate = nums[i];
+                vote = 1;
             }
         }
         return candidate;
