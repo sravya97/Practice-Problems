d04abd68cb36c45a88974bd38674a5106c5f661e
sravya97 <sravya.gopireddy09@gmail.com>
2025-12-31 12:13:44 -0600

    Time: 20 ms (62.34%), Space: 45.7 MB (77.29%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,28 +1,37 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-        for(int i=0;i<nums.length-3;i++){
-            for(int j=i+1; j<nums.length-2;j++){
-                List<Integer> curr = new ArrayList<>();
-                int start = j+1;
-                int end = nums.length-1;
-                while(start < end){
-                    long sum = (long) nums[i] + nums[j] + nums[start] + nums[end];
-                    if(sum == target){
-                        set.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
-                        start++;
-                        end--;
+        Arrays.sort(nums);
+        for(int i=0;i < nums.length-3; i++){
+            if(i-1 >=0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            
+            for(int j=i+1; j < nums.length-2; j++){
+                if(j-1 != i && nums[j] == nums[j-1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while(left < right){
+                   
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if(target == sum){
+                        result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                        while ((left < right) && (nums[left + 1] == nums[left])) left++;
+                        while ((left < right) && (nums[right - 1] == nums[right])) right--;
+                        
+                        left++;
+                        right --;
                     } else if(sum < target){
-                        start ++;
+                        
+                        left ++;
                     } else {
-                        end--;
+                        right --;
                     }
                 }
             }
         }
-        result.addAll(set);
         return result;
     }
 }
