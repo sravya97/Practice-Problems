669f913c6ad46436524b45aec625047d7fbeaffb
sravya97 <sravya.gopireddy09@gmail.com>
2026-01-01 17:59:18 -0600

    Time: 2 ms (68.89%), Space: 45.5 MB (8.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        recursive(n, 0, 0, "", result);
+        backtrack(n, result, 0, 0, "");
         return result;
     }
-    
-    public void recursive(int n, int open, int close, String current, List<String> result){
-        if(open == n && close == n){
-            result.add(current);
+
+    public void backtrack(int n, List<String> result, int open, int closed, String curr){
+        if(open == closed && open == n){
+            result.add(curr);
             return;
         }
         if(open < n){
-            recursive(n, open+1, close, current + "(", result);
+            curr += "(";
+            backtrack(n, result, open+1, closed, curr);
+            curr = curr.substring(0, curr.length()-1);
         }
-        if(open > close){
-            recursive(n, open, close+1, current + ")", result);
+        if(closed < open){
+            curr += ")";
+            backtrack(n, result, open, closed+1, curr);
+            curr = curr.substring(0, curr.length()-1);
         }
-        return;
     }
 }
